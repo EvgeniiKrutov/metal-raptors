a1296db0f39bcfdc5e1af418a1b095747ff342c6
EvgeniiKrutov <evgenii.krutov.europe@gmail.com>
2026-07-10 12:50:56 +0200

    MR: Add sound tracks to the game

--- src/game/config/data/music/index.ts
+++ src/game/config/data/music/index.ts
@@ -1,8 +1,30 @@
 import airAssault from './air-assault.json';
+import barnstormerWaltz from './barnstormer-waltz.json';
+import brassBattalion from './brass-battalion.json';
+import dreadLegion from './dread-legion.json';
+import ironRequiem from './iron-requiem.json';
+import ironSkies from './iron-skies.json';
+import obsidianMarch from './obsidian-march.json';
+import steelTalons from './steel-talons.json';
+import raptorMarch from './raptor-march.json';
+import squadronSwing from './squadron-swing.json';
+import flakParade from './flak-parade.json';
+import thunderRun from './thunder-run.json';
 import { MusicConfig } from '../../../../types/game.types';
 
 export const MUSIC_TRACKS: MusicConfig[] = [
   airAssault as unknown as MusicConfig,
+  barnstormerWaltz as unknown as MusicConfig,
+  brassBattalion as unknown as MusicConfig,
+  dreadLegion as unknown as MusicConfig,
+  ironRequiem as unknown as MusicConfig,
+  ironSkies as unknown as MusicConfig,
+  obsidianMarch as unknown as MusicConfig,
+  steelTalons as unknown as MusicConfig,
+  raptorMarch as unknown as MusicConfig,
+  squadronSwing as unknown as MusicConfig,
+  flakParade as unknown as MusicConfig,
+  thunderRun as unknown as MusicConfig,
 ];
 
 export function getMusicById(id: string): MusicConfig | undefined {
